0fb5ce83c860ebb46f51079c4c1fb21bac9645fd
@jiqiaochun <13011251196@163.com>
2017-05-10 15:16:11 +0800

    fff

--- testOpenSetting/ViewController.m
+++ testOpenSetting/ViewController.m
@@ -16,7 +16,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    [btn setBackgroundColor:[UIColor redColor]];
+    [btn setTitle:@"打开设置--通知" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+}
+
+/**  设置地址
+ 
+ About — prefs:root=General&path=About
+ Accessibility — prefs:root=General&path=ACCESSIBILITY
+ Airplane Mode On — prefs:root=AIRPLANE_MODE
+ Auto-Lock — prefs:root=General&path=AUTOLOCK
+ Brightness — prefs:root=Brightness
+ Bluetooth — prefs:root=General&path=Bluetooth
+ Date & Time — prefs:root=General&path=DATE_AND_TIME
+ FaceTime — prefs:root=FACETIME
+ General — prefs:root=General
+ Keyboard — prefs:root=General&path=Keyboard
+ iCloud — prefs:root=CASTLE
+ iCloud Storage & Backup — prefs:root=CASTLE&path=STORAGE_AND_BACKUP
+ International — prefs:root=General&path=INTERNATIONAL
+ Location Services — prefs:root=LOCATION_SERVICES
+ Music — prefs:root=MUSIC
+ Music Equalizer — prefs:root=MUSIC&path=EQ
+ Music Volume Limit — prefs:root=MUSIC&path=VolumeLimit
+ Network — prefs:root=General&path=Network
+ Nike + iPod — prefs:root=NIKE_PLUS_IPOD
+ Notes — prefs:root=NOTES
+ Notification — prefs:root=NOTIFICATIONS_ID
+ Phone — prefs:root=Phone
+ Photos — prefs:root=Photos
+ Profile — prefs:root=General&path=ManagedConfigurationList
+ Reset — prefs:root=General&path=Reset
+ Safari — prefs:root=Safari
+ Siri — prefs:root=General&path=Assistant
+ Sounds — prefs:root=Sounds
+ Software Update — prefs:root=General&path=SOFTWARE_UPDATE_LINK
+ Store — prefs:root=STORE
+ Twitter — prefs:root=TWITTER
+ Usage — prefs:root=General&path=USAGE
+ VPN — prefs:root=General&path=Network/VPN
+ Wallpaper — prefs:root=Wallpaper
+ Wi-Fi — prefs:root=WIFI
+ 
+ */
+
+- (void)btnClick:(UIButton *)btn{
+    NSURL *url = [NSURL URLWithString:@"prefs:root=NOTIFICATIONS_ID"];
+    if ([[UIApplication sharedApplication] canOpenURL:url])
+    {
+        [[UIApplication sharedApplication] openURL:url];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
